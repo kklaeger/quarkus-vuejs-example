138cff093f18f2c868a90203f578e70a8345a9bd
Konstantin Klaeger <kklaeger@users.noreply.github.com>
2020-08-27 07:29:23 +0200

    Update unit test and add script to update snapshots

--- vuejs-frontend/tests/unit/UserList.spec.ts
+++ vuejs-frontend/tests/unit/UserList.spec.ts
@@ -2,7 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import UserList from "@/components/UserList.vue";
 
 describe(UserList.name + " component", () => {
-  it("should render successfully.", () => {
+  it("should render and match snapshot.", () => {
     const wrapper = shallowMount(UserList);
     expect(wrapper).toMatchSnapshot();
   });
